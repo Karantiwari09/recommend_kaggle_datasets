5b10f9416a9ac5664e537f48a45b19b9c95b849e
Karantiwari09 <karantiwri740@gmail.com>
2025-06-30 15:52:39 +0530

    Update app to use Render port

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
+import os
 from flask import Flask, render_template, request
 import joblib
-import numpy as np
 
 app = Flask(__name__)
-
 model, vectorizer, dataset = joblib.load("model/recommender_model.pkl")
 
 @app.route("/")
@@ -13,11 +12,8 @@ def home():
 @app.route("/recommend", methods=["POST"])
 def recommend():
     user_input = request.form["interest"]
-
     input_vec = vectorizer.transform([user_input])
-
-    distances, indices = model.kneighbors(input_vec, n_neighbors=20) 
-
+    distances, indices = model.kneighbors(input_vec, n_neighbors=20)
     matched_data = dataset.iloc[indices[0]][[
         "Dataset_name", "Author_name", "Type_of_file", "Medals", "size", "Dataset_link"
     ]].reset_index(drop=True)
@@ -25,4 +21,45 @@ def recommend():
     return render_template("result.html", interest=user_input, recommendations=matched_data.to_dict(orient="records"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, render_template, request
+# import joblib
+# import numpy as np
+
+# app = Flask(__name__)
+
+# model, vectorizer, dataset = joblib.load("model/recommender_model.pkl")
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/recommend", methods=["POST"])
+# def recommend():
+#     user_input = request.form["interest"]
+
+#     input_vec = vectorizer.transform([user_input])
+
+#     distances, indices = model.kneighbors(input_vec, n_neighbors=20) 
+
+#     matched_data = dataset.iloc[indices[0]][[
+#         "Dataset_name", "Author_name", "Type_of_file", "Medals", "size", "Dataset_link"
+#     ]].reset_index(drop=True)
+
+#     return render_template("result.html", interest=user_input, recommendations=matched_data.to_dict(orient="records"))
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
