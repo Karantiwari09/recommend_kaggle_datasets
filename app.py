d28224b7364fa167c2cc229cdf1f40740aca5787
Karantiwari09 <karantiwri740@gmail.com>
2025-07-01 12:24:08 +0530

    top 10

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
 def recommend():
     user_input = request.form["interest"]
     input_vec = vectorizer.transform([user_input])
-    distances, indices = model.kneighbors(input_vec, n_neighbors=20)
+    distances, indices = model.kneighbors(input_vec, n_neighbors=10)
     matched_data = dataset.iloc[indices[0]][[
         "Dataset_name", "Author_name", "Type_of_file", "Medals", "size", "Dataset_link"
     ]].reset_index(drop=True)
